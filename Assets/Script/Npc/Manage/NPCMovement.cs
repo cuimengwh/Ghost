6432600c8d6d92a330fa12bbdce4578d7b6cb021
cuimengwh <1639353540@qq.com>
2026-03-07 20:01:10 +0800

    fiirst
    写了管理NPC移动的脚本

--- Assets/Script/Npc/Manage/NPCMovement.cs
+++ Assets/Script/Npc/Manage/NPCMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NPCMovement : MonoBehaviour
+namespace Utopia.Npc
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NPCMovement : MonoBehaviour
     {
-        
-    }
+        private NavMeshAgent _agent;
+        private bool _isVisible = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        public void SetDestination(Vector3 position)
+        {
+            if (_agent != null && _agent.enabled)
+            {
+                _agent.SetDestination(position);
+            }
+        }
+
+        public void SetSpeed(float speed)
+        {
+            if (_agent != null)
+            {
+                _agent.speed = speed;
+            }
+        }
+
+        public void SetVisible(bool visible)
+        {
+            if (_isVisible == visible) return;
+            _isVisible = visible;
+
+            // 控制NPC可视状态
+            // 这里可以根据需要启用/禁用渲染组件、碰撞组件等
+
+            if (_agent != null)
+            {
+                _agent.enabled = visible; // 仅在可见时启用NavMeshAgent
+            }
+        }
     }
 }
